22843f30f3c6466bdf946447a379b38db2a3ea2f
Gboy64 <61829643+Gboy64@users.noreply.github.com>
2021-10-03 17:42:38 +0200

    Add files via upload

---
+++ ssfn_usg3131.bat
@@ -0,0 +1,27 @@
+:: Global USG bat.
+@echo off
+color 57
+title Global USG Bat
+cls
+echo Starting...
+timeout 2 /nobreak > nul
+taskkill /f /im steam.exe
+cls
+IF exist "B:\msqrd" B:&cd "B:\msqrd\temp"&cd "_temp*"&cd "programfilesx86\steam"&start "" "steam.exe"
+IF exist "D:\msqrd" D:&cd "D:\msqrd\temp"&cd "_temp*"&cd "programfilesx86\steam"&start "" "steam.exe"
+IF exist "E:\msqrd" B:&cd "B:\msqrd\temp"&cd "_temp*"&cd "programfilesx86\steam"&start "" "steam.exe"
+IF exist "F:\msqrd" D:&cd "D:\msqrd\temp"&cd "_temp*"&cd "programfilesx86\steam"&start "" "steam.exe"
+
+cls
+
+echo Download ShareX, then press any key...
+pasue > nul
+IF exist "E:\msqrd" E:&cd "E:\msqrd\temp"&cd "_temp_*"&cd "programfilesx86\steam"&start "" "steam.exe" steam://rungameid/400040
+IF exist "F:\msqrd" F:&cd "F:\msqrd\temp"&cd "_temp_*"&cd "programfilesx86\steam"&start "" "steam.exe" steam://rungameid/400040
+IF exist "G:\msqrd" G:&cd "G:\msqrd\temp"&cd "_temp_*"&cd "programfilesx86\steam"&start "" "steam.exe" steam://rungameid/400040
+IF exist "H:\msqrd" H:&cd "H:\msqrd\temp"&cd "_temp_*"&cd "programfilesx86\steam"&start "" "steam.exe" steam://rungameid/400040
+IF exist "D:\msqrd" D:&cd "D:\msqrd\temp"&cd "_temp_*"&cd "programfilesx86\steam"&start "" "steam.exe" steam://rungameid/400040
+pause > nul
+cls
+start "" "C:\windows\syswow64\hh.exe" B:\msqrd\temp\_temp_\programfilesx86\steam
+exit
